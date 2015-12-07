9432d4502b266e7d3c5d940fa5e68ff4b73206e4
Marcelo Lazaroni <lazaronijunior@gmail.com>
2015-12-07 18:04:36 +0000

    Fix file loading

--- js/canvas360.js
+++ js/canvas360.js
@@ -1,4 +1,5 @@
-/*globals $, document, readyIcons*/
+/*globals $, document, readyIcons, FileReader*/
+var zoom = 0.5;
 $(document).ready(function () {
   "use strict";
   var images = [],
@@ -19,13 +20,40 @@ $(document).ready(function () {
       "13_Speciality_Restaurant"
     ],
     $canvas = $('canvas'),
+    canvas = document.querySelector("canvas"),
     currentFrame,
     out = document.querySelector("#console"),
     frameNo = document.querySelector('#frame-number'),
     demoImage = document.querySelector("#demo-img"),
     modal = document.querySelector("#modal"),
     dragging = false,
-    draggingDistance = 30;
+    draggingDistance = 50,
+    checkDragging,
+    recording = false;
+
+  function downloadFile() {
+    var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(icons)),
+      a = document.createElement('a');
+    a.href = 'data:' + data;
+    a.download = 'data.json';
+    a.innerHTML = 'Download coordinates';
+    document.body.appendChild(a);
+  }
+
+  function readSingleFile(e) {
+    var file = e.target.files[0],
+      reader;
+    if (!file) {
+      return;
+    }
+    reader = new FileReader();
+    reader.onload = function (e) {
+      var contents = e.target.result;
+      icons = JSON.parse(contents);
+      console.log(icons);
+    };
+    reader.readAsText(file);
+  }
 
   function output(text) {
     var p = document.createElement("p");
@@ -42,14 +70,22 @@ $(document).ready(function () {
     ctx.drawImage(images[frame], 0, 0);
   }
 
-  function loadImages() {
-    var i;
+  function loadImages(callback) {
+    var i,
+      loadedImages = 0;
     //Load frame images
+    function imgloaded() {
+      loadedImages += 1;
+      if (loadedImages === 36) {
+        callback();
+      }
+    }
     for (i = 0; i < 36; i++) {
       images[i] = new Image();
       images[i].src = "img/img" + i + ".jpg";
+      images[i].onload = imgloaded;
     }
-    drawFrame(0, document.querySelector("canvas"));
+    drawFrame(0, canvas);
   }
 
   function highlightInGuide(pointerNo) {
@@ -78,18 +114,14 @@ $(document).ready(function () {
     highlightInGuide(0);
   }
 
-
-
   function nxtFrame(canvasElement, prevFrame) {
     if (currentFrame === undefined) {
       currentFrame = 35;
     } else if (prevFrame) {
       currentFrame = Math.abs((35 + currentFrame) % 36);
-      console.log("prev");
     } else {
       currentFrame = Math.abs((37 + currentFrame) % 36);
     }
-    console.log(currentFrame);
     drawFrame(currentFrame, canvasElement);
     frameNo.innerHTML = currentFrame;
   }
@@ -118,100 +150,15 @@ $(document).ready(function () {
     };
   }
 
-  function stopRecording() {
-    $canvas.off("mousedown");
-    $canvas.off("mouseup");
-  }
-
-  function startRecording() {
-    var currentPointer = 0;
-    showPointersGuide();
-    clearOutput();
-    $canvas.on("mousedown", function canvasMouseDown(event) {
-      var pos = getPosition(event, this);
-      icons[currentPointer][currentFrame].x = pos.x;
-      icons[currentPointer][currentFrame].y = pos.y;
-    });
-
-    $canvas.on("mouseup", function canvasMouseUp(event) {
-      var pos = getPosition(event, this);
-      icons[currentPointer][currentFrame].w = pos.x - icons[currentPointer][currentFrame].x;
-      icons[currentPointer][currentFrame].h = pos.y - icons[currentPointer][currentFrame].y;
-
-      output("Recorded data for " + iconFolders[currentPointer] + " for frame " + currentFrame);
-      if (currentPointer + 1 < icons.length) {
-        currentPointer += 1;
-        highlightInGuide(currentPointer);
-      } else if (currentFrame < 35) {
-        stopRecording();
-        output("Frame finished.");
-      } else {
-        stopRecording();
-        output("Everything finished");
-        output(JSON.stringify(icons));
-      }
-    });
-  }
-
-  $("#nxt-frame").on("mousedown", function () {
-    // stopRecording();
-    nxtFrame(document.querySelector("canvas"));
-    // startRecording();
-  });
-
-  $("#reset").on("mousedown", function () {
-    stopRecording();
-    startRecording();
-  });
-
-  $("#load").on("mousedown", function () {
-    var stag = document.createElement("script");
-    stag.src = "js/icons.js";
-    document.body.appendChild(stag);
-    stag.onload = function () {
-      icons = readyIcons;
-    };
-    currentFrame = 1;
-    drawFrame(1, document.querySelector("canvas"));
-  });
-
-  function checkDragging(event) {
-    var lastCheck = new Date(),
-      lastPosition = getPosition(event, event.target),
-      diff,
-      currPos;
-    dragging = true;
-    checkDragging = function (event) {
-      if (new Date() - lastCheck > 50) {
-        lastCheck = new Date();
-        if (dragging === true) {
-          currPos = getPosition(event, event.target);
-          diff = lastPosition.x - currPos.x;
-        } else {
-          dragging = true;
-          diff = 0;
-        }
-        // If dragged for long enough.
-        if (Math.abs(diff) > draggingDistance) {
-          lastPosition = getPosition(event, event.target);
-          if (diff > 0) { //dragging to the right;
-            console.log("right");
-            nxtFrame(event.target);
-          } else { //dragging to the left;
-            console.log("left");
-            nxtFrame(event.target, true);
-          }
-        }
-      }
-    };
-    checkDragging(event);
-  }
-  //When in standby, this is the function that will be handling the canvas
   function handleMouseDown(event) {
     var i,
       ico,
       pos = getPosition(event, this),
       pointerClick = false;
+
+    //Take zoom into account
+    pos.x = pos.x * (1 / zoom);
+    pos.y = pos.y * (1 / zoom);
     //Check if the click was overy any of the pointers
     for (i = 0; i < icons.length; i++) {
       ico = icons[i][currentFrame];
@@ -234,6 +181,106 @@ $(document).ready(function () {
       });
     }
   }
+
+  function stopRecording() {
+    $canvas.off("mousedown");
+    $canvas.off("mouseup");
+  }
+
+  function startRecording() {
+    var currentPointer = 0;
+    showPointersGuide();
+    clearOutput();
+    $('#reset')[0].className = "";
+    $canvas.off("mousedown");
+    $canvas.on("mousedown", function canvasMouseDown(event) {
+      var pos = getPosition(event, this);
+      icons[currentPointer][currentFrame].x = pos.x;
+      icons[currentPointer][currentFrame].y = pos.y;
+    });
+
+    $canvas.on("mouseup", function canvasMouseUp(event) {
+      var pos = getPosition(event, this);
+      icons[currentPointer][currentFrame].w = pos.x - icons[currentPointer][currentFrame].x;
+      icons[currentPointer][currentFrame].h = pos.y - icons[currentPointer][currentFrame].y;
+
+      output("Recorded data for " + iconFolders[currentPointer] + " for frame " + currentFrame);
+      if (currentPointer + 1 < icons.length) {
+        currentPointer += 1;
+        highlightInGuide(currentPointer);
+      } else if (currentFrame < 35) {
+        stopRecording();
+        output("Frame finished.");
+      } else {
+        stopRecording();
+        $('#reset')[0].className = "hidden";
+        $('#nxt-frame').innerHTML = "Start Recording";
+        $canvas.on("mousedown", handleMouseDown);
+        output("Everything finished");
+        output(JSON.stringify(icons));
+        downloadFile();
+      }
+    });
+  }
+
+  $("#nxt-frame").on("mousedown", function () {
+    if (!recording) {
+      var i, j;
+      //prepare icons object
+      for (i = 0; i < 13; i++) {
+        icons[i] = [];
+        for (j = 0; j < 36; j++) {
+          icons[i][j] = {};
+        }
+      }
+      currentFrame = 35;
+      nxtFrame(canvas);
+      startRecording();
+      recording = true;
+      this.innerHTML = "Next Frame";
+    } else {
+      stopRecording();
+      nxtFrame(canvas);
+      startRecording();
+    }
+  });
+
+  $("#reset").on("mousedown", function () {
+    stopRecording();
+    startRecording();
+  });
+
+  $("#file-input").on("change", readSingleFile);
+
+  checkDragging = (function () {
+    var lastCheck = new Date(),
+      lastPosition = {x: 0},
+      diff,
+      currPos;
+    return function (event) {
+      if (new Date() - lastCheck > 0) {
+        lastCheck = new Date();
+        if (dragging === true) {
+          currPos = getPosition(event, event.target);
+          diff = lastPosition.x - currPos.x;
+        } else {
+          dragging = true;
+          diff = 0;
+        }
+        // If dragged for long enough.
+        if (Math.abs(diff) > draggingDistance) {
+          lastPosition = getPosition(event, event.target);
+          if (diff > 0) { //dragging to the right;
+            nxtFrame(event.target);
+          } else { //dragging to the left;
+            nxtFrame(event.target, true);
+          }
+        }
+      }
+    };
+  }());
+  //When in standby, this is the function that will be handling the canvas
+
   $canvas.on("mousedown", handleMouseDown);
 
   $('#modal').on("mousedown", function () {
@@ -243,5 +290,10 @@ $(document).ready(function () {
   });
 
 
-  loadImages();
+  loadImages(function () {
+    nxtFrame(canvas, 0);
+  });
 });
+
+//To be removed
+window.parent.document.body.style.zoom = zoom;
