322a610c8b9f57aa23dbd08b3b61c7152cb35367
Jonathan Schmid <hi@jonathanschmid.de>
2015-12-16 12:43:46 +0000

    Use slider instead of dragging

--- js/canvas360.js
+++ js/canvas360.js
@@ -1,5 +1,5 @@
-/* globals window, document, $, console */
-(function (window, document, $, console) {
+/* globals window, document, console */
+(function (window, document, console) {
   'use strict';
 
   var xmlHttp,
@@ -8,23 +8,9 @@
 
   document.addEventListener('DOMContentLoaded', function () {
     var images = [],
-      icons = [],
-      $canvas = $('canvas'),
       canvas = document.querySelector('canvas'),
-      currentFrame,
-      out = document.querySelector('#console'),
-      frameNo = document.querySelector('#frame-number'),
-      demoImage = document.querySelector('#demo-img'),
-      dragging = false,
-      draggingDistance = 50,
-      checkDragging,
-      recording = false;
-
-    function output (text) {
-      var p = document.createElement('p');
-      p.innerHTML = text;
-      out.appendChild(p);
-    }
+      currentFrame = 0,
+      demoImage = document.querySelector('#demo-img');
 
     function drawFrame (frame, canvasElement) {
       var ctx = canvasElement.getContext('2d');
@@ -52,50 +38,25 @@
       drawFrame(0, canvas);
     }
 
-    function highlightInGuide (pointerNo) {
-      if (pointerNo > 0) {
-        $('#pointers-guide li:nth-child(' + pointerNo + ')')[0].style.fontWeight = 'normal';
-      }
-      $('#pointers-guide li:nth-child(' + (pointerNo + 1) + ')')[0].style.fontWeight = 'bold';
-    }
+    function cumulativeOffset (element) {
+      var top = 0,
+        left = 0;
 
-    function showPointersGuide () {
-      var i,
-        frag = document.createDocumentFragment(),
-        li,
-        guide;
+      do {
+        top += element.offsetTop  || 0;
+        left += element.offsetLeft || 0;
+        element = element.offsetParent;
+      } while(element);
 
-      for (i = 0; i < iconFolders.length; i++) {
-        li = document.createElement('li');
-        li.id = iconFolders[i];
-        li.innerHTML = iconFolders[i];
-        frag.appendChild(li);
-      }
-
-      guide = document.querySelector('#pointers-guide');
-
-      while (guide.firstChild) {
-        guide.removeChild(guide.firstChild);
-      }
-
-      guide.appendChild(frag);
-      highlightInGuide(0);
-    }
-
-    function nxtFrame (canvasElement, prevFrame) {
-      if (currentFrame === undefined) {
-        currentFrame = 35;
-      } else if (prevFrame) {
-        currentFrame = Math.abs((35 + currentFrame) % 36);
-      } else {
-        currentFrame = Math.abs((37 + currentFrame) % 36);
-      }
-      drawFrame(currentFrame, canvasElement);
-      frameNo.innerHTML = currentFrame;
+      return {
+        top: top,
+        left: left
+      };
     }
 
     function getPosition (event, canvasElement) {
-      var x, y;
+      var x, y,
+        offset = cumulativeOffset(canvasElement);
 
       if (event.x !== undefined && event.y !== undefined) {
         x = event.x;
@@ -105,8 +66,8 @@
         y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
       }
 
-      x -= canvasElement.offsetLeft;
-      y -= canvasElement.offsetTop;
+      x -= offset.left;
+      y -= offset.top;
 
       x -= window.pageXOffset;
       y -= window.pageYOffset;
@@ -117,11 +78,10 @@
       };
     }
 
-    function handleMouseDown (event) {
+    function handleClick (event) {
       var iconFolder,
         icon,
-        clickPos = getPosition(event, this),
-        pointerClick = false;
+        clickPos = getPosition(event, this);
 
       //Check if the click was overy any of the pointers
       for (iconFolder in config[currentFrame]) {
@@ -135,82 +95,29 @@
         if (clickPos.x >= icon.x1 && clickPos.x <= icon.x2 && clickPos.y >= icon.y1 && clickPos.y <= icon.y2) {
           demoImage.src = 'img/Icons/'+iconFolder+'/'+iconFolder+'.jpg';
           document.querySelector('#modal').classList.add('visible');
-          pointerClick = true;
           break;
         }
       }
-
-      if (!pointerClick) {
-        $canvas.mousemove(checkDragging);
-        $canvas.on('mouseup', function quitDragging () {
-          $canvas.off('mousemove');
-          dragging = false;
-        });
-      }
     }
 
-    $('#nxt-frame').on('mousedown', function () {
-      if (!recording) {
-        var i, j;
-        //prepare icons object
-        for (i = 0; i < 13; i++) {
-          icons[i] = [];
-          for (j = 0; j < 36; j++) {
-            icons[i][j] = {};
-          }
-        }
-        currentFrame = 35;
-        nxtFrame(canvas);
-        startRecording();
-        recording = true;
-        this.innerHTML = 'Next Frame';
-      } else {
-        stopRecording();
-        nxtFrame(canvas);
-        startRecording();
-      }
-    });
-
-    checkDragging = (function () {
-      var lastCheck = new Date(),
-        lastPosition = {x: 0},
-        diff,
-        currPos;
-
-      return function (event) {
-        if (new Date() - lastCheck > 15) {
-          lastCheck = new Date();
-          if (dragging === true) {
-            currPos = getPosition(event, event.target);
-            diff = lastPosition.x - currPos.x;
-          } else {
-            dragging = true;
-            diff = 0;
-          }
-          // If dragged for long enough.
-          if (Math.abs(diff) > draggingDistance) {
-            lastPosition = getPosition(event, event.target);
-            if (diff > 0) { //dragging to the right;
-              nxtFrame(event.target);
-            } else { //dragging to the left;
-              nxtFrame(event.target, true);
-            }
-          }
-        }
-      };
-    }());
     //When in standby, this is the function that will be handling the canvas
+    canvas.addEventListener('click', handleClick);
 
-    $canvas.on('mousedown', handleMouseDown);
-
-    $('#modal').on('mousedown', function () {
+    document.querySelector('#modal').addEventListener('click', function () {
       this.classList.remove('visible');
     });
 
+    document.querySelector('input[type=range]').addEventListener('input', function (e) {
+      currentFrame = e.target.value;
+      console.log(currentFrame);
+      drawFrame(currentFrame, canvas);
+    }, false);
+
 
     loadImages(function () {
-      nxtFrame(canvas, 0);
+      drawFrame(currentFrame, canvas);
     });
+
   });
 
   xmlHttp = new XMLHttpRequest();
@@ -219,4 +126,4 @@
   config = JSON.parse(xmlHttp.responseText);
   iconFolders = Object.keys(config[Object.keys(config)[0]]);
 
-}(window, document, jQuery, console));
+}(window, document, console));
